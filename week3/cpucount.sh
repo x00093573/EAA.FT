20ab5c3695c5efcac81e9118d0f9d441c7c77874
x00093573 <x00093573@ittd.ie>
2014-10-03 11:45:26 +0100

    Week 2 Lab (b)

---
+++ week3/cpucount.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#: Title : CPU Info
+#: Date : 03/10/2014
+#: Author : Sean Campbell
+#: Version :1.0
+#: Description : Code for processor count
+#: Options : ..
+
+
+
+
+
+grep processor /proc/cpuinfo | wc -l
+
+
